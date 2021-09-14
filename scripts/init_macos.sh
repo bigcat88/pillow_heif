4a6283ad62d2096d711d5c949d183254589a9f50
Alexander Piskun <bigcat88@icloud.com>
2021-09-15 02:52:37 +0300

    Fix for macOS arm.

--- scripts/init_macos.sh
+++
@@ -1,9 +0,0 @@
-set -ex \
-    && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-set -ex \
-    && brew install libffi libheif
-
-if [[ -z "${HOMEBREW_PREFIX}" ]]; then
-  HOMEBREW_PREFIX=$(brew --prefix)
-fi
