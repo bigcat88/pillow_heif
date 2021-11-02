d2efad76c7f0935148e6c12a829665975c19562e
Alexander Piskun <bigcat88@icloud.com>
2021-11-02 22:36:32 +0200

    test

--- build_scripts/linux/pkg-config.sh
+++ build_scripts/linux/pkg-config.sh
@@ -1,1 +1,9 @@
-echo "build__pkg-config.sh"
+cd /host/build-tools \
+&& PKG_CONFIG_VERSION="0.29.2" \
+&& wget --no-check-certificate https://pkg-config.freedesktop.org/releases/pkg-config-${PKG_CONFIG_VERSION}.tar.gz \
+&& tar xvf pkg-config-${PKG_CONFIG_VERSION}.tar.gz \
+&& cd pkg-config-${PKG_CONFIG_VERSION} \
+&& ./configure --with-internal-glib \
+&& make -j4 \
+&& make install \
+&& pkg-config --version
