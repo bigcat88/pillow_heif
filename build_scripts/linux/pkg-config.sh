d5e9f3390a57ed45d0cb44ea63251603a2b3586f
Alexander Piskun <bigcat88@icloud.com>
2021-11-02 23:51:13 +0200

    test

--- build_scripts/linux/pkg-config.sh
+++ build_scripts/linux/pkg-config.sh
@@ -1,8 +1,15 @@
-cd /host/build-tools \
-&& wget --no-check-certificate https://pkg-config.freedesktop.org/releases/pkg-config-"$1".tar.gz \
-&& tar xvf pkg-config-"$1".tar.gz \
-&& cd pkg-config-"$1" \
-&& ./configure --with-internal-glib \
-&& make -j4 \
-&& make install \
+cd /host/build-tools || exit 2
+ls -la
+if [[ -d pkg-config-"$1" ]]
+then
+  echo "No cache found, build it..."
+  wget --no-check-certificate https://pkg-config.freedesktop.org/releases/pkg-config-"$1".tar.gz \
+  && tar xvf pkg-config-"$1".tar.gz \
+  && cd pkg-config-"$1" \
+  && ./configure --with-internal-glib \
+  && make -j4
+else
+  echo "Cache found, install it..."
+fi
+make install \
 && pkg-config --version
