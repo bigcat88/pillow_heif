f908c1ec6c295d7eabdbd91abef06f4e6b0979c9
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 00:06:04 +0200

    test

--- build_scripts/linux/autoconf.sh
+++ build_scripts/linux/autoconf.sh
@@ -1,1 +1,15 @@
-echo "build__autoconf.sh"
+cd /host/build-tools || exit 2
+ls -la
+if [[ -d autoconf-"$1" ]]
+then
+  echo "No cache found for autoconf, build it..."
+  wget --no-check-certificate https://ftp.gnu.org/gnu/autoconf/autoconf-${AUTOCONF_VERSION}.tar.gz \
+  && tar xvf autoconf-"$1".tar.gz \
+  && cd autoconf-"$1" \
+  && ./configure \
+  && make -j4
+else
+  echo "Cache found for autoconf, install it..."
+fi
+make install \
+&& autoconf --version
--- build_scripts/linux/pkg-config.sh
+++ build_scripts/linux/pkg-config.sh
@@ -2,14 +2,14 @@ cd /host/build-tools || exit 2
 ls -la
 if [[ -d pkg-config-"$1" ]]
 then
-  echo "No cache found, build it..."
+  echo "No cache found for pkg-config, build it..."
   wget --no-check-certificate https://pkg-config.freedesktop.org/releases/pkg-config-"$1".tar.gz \
   && tar xvf pkg-config-"$1".tar.gz \
   && cd pkg-config-"$1" \
   && ./configure --with-internal-glib \
   && make -j4
 else
-  echo "Cache found, install it..."
+  echo "Cache found for pkg-config, install it..."
 fi
 make install \
 && pkg-config --version
