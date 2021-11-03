ec1bd2f433a5989f237d9f809709a88f418d2d6d
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 11:19:25 +0200

    test

--- build_scripts/linux/pkg-config.sh
+++ build_scripts/linux/pkg-config.sh
@@ -8,7 +8,7 @@ echo $(ls -la /host/build-tools)
 echo "ATTENTION3"
 echo $(ls -la "/host/build-tools/pkg-config-$1")
 echo "pkg-config-$1/"
-if [[ -d "./pkg-config-$1/" ]]
+if [[ -d "/host/build-tools/pkg-config-$1/" ]]
  then
   echo "No cache found for pkg-config, build it..."
   wget --no-check-certificate "https://pkg-config.freedesktop.org/releases/pkg-config-$1.tar.gz" \
