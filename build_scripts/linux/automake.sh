c04b74ba1d7c769e15e92fb1876f3465f1b3b5bf
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 13:36:02 +0200

    test

--- build_scripts/linux/automake.sh
+++ build_scripts/linux/automake.sh
@@ -4,7 +4,7 @@ if [[ -d "automake-$1" ]]; then
   cd "automake-$1" || exit 102
 else
   echo "No cache found for automake, build it..."
-  wget -q --no-check-certificate "https://ftp.gnu.org/gnu/autoconf/automake-$1.tar.gz" \
+  wget -q --no-check-certificate "https://ftp.gnu.org/gnu/automake/automake-$1.tar.gz" \
   && tar xvf "automake-$1.tar.gz" \
   && cd "automake-$1" \
   && ./configure \
--- build_scripts/linux/libressl.sh
+++ build_scripts/linux/libressl.sh
@@ -1,10 +1,11 @@
-set -ex \
-    && cd /build-tools \
-    && LIBRESSL_VERSION="3.1.5" \
-    && wget https://github.com/libressl-portable/portable/archive/v${LIBRESSL_VERSION}.tar.gz \
-    && tar xvf v${LIBRESSL_VERSION}.tar.gz \
-    && cd portable-${LIBRESSL_VERSION} \
-    && ./autogen.sh \
-    && ./configure \
-    && make -j4 \
-    && make install
+cd /host/build-deps || exit 2
+if [[ -d "libressl-$1" ]]; then
+  echo "Cache found for LibreSSL, install it..."
+  cd "libressl-$1" || exit 102
+else
+  echo "No cache found for LibreSSL, build it..."
+  wget -O "libressl-$1.tar.gz" "https://github.com/libressl-portable/portable/archive/v$1.tar.gz" \
+  && tar xvf "libressl.tar.gz" --one-top-level \
+  && echo $(ls -la)
+fi
+make install
