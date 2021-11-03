db9dc8427921bb7be3c587f17a0ab5d89d86eabd
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 19:37:08 +0200

    test

--- build_scripts/linux/aom.sh
+++ build_scripts/linux/aom.sh
@@ -1,14 +1,14 @@
 NAME=$(basename "$BASH_SOURCE" | cut -f 1 -d '.')
 cd /host/build-deps || exit 2
-if [[ -d "$NAME-$1" ]]; then
+if [[ -d "$NAME-build-$1" ]]; then
   echo "Cache found for lib$NAME, install it..."
   cd "$NAME-build-$1" || exit 102
 else
   echo "No cache found for lib$NAME, build it..."
-  mkdir "$NAME-$1" "$NAME-build-$1" && cd "$NAME-$1" || exit 104
-  wget -q --no-check-certificate -O "$NAME-$1.tar.gz" "https://aomedia.googlesource.com/aom/+archive/$1.tar.gz" \
+  mkdir "$NAME-build-$1"
+  wget --no-check-certificate -O "$NAME-$1.tar.gz" "https://aomedia.googlesource.com/aom/+archive/$1.tar.gz" \
   && ls -la \
-  && tar xvf "$NAME-$1.tar.gz" -C "$NAME-build-$1" --strip-components 1 \
+  && tar xvf "$NAME-$1.tar.gz" \
   && ls -la \
   && cd "$NAME-build-$1" \
   && ls -la \
--- build_scripts/linux/libheif.sh
+++ build_scripts/linux/libheif.sh
@@ -1,10 +1,14 @@
-#set -ex \
-#    && cd /build-deps \
-#    && LIBHEIF_VERSION="1.12.0" \
-#    && wget https://github.com/strukturag/libheif/releases/download/v${LIBHEIF_VERSION}/libheif-${LIBHEIF_VERSION}.tar.gz \
-#    && tar xvf libheif-${LIBHEIF_VERSION}.tar.gz \
-#    && cd libheif-${LIBHEIF_VERSION} \
-#    && ./configure --prefix /usr \
-#    && make -j4 \
-#    && make install \
-#    && ldconfig
+NAME=$(basename "$BASH_SOURCE" | cut -f 1 -d '.')
+cd /host/build-deps || exit 2
+if [[ -d "$NAME-$1" ]]; then
+  echo "Cache found for $NAME, install it..."
+  cd "$NAME-$1" || exit 102
+else
+  echo "No cache found for $NAME, build it..."
+  wget -q "https://github.com/strukturag/libheif/releases/download/v$1/$NAME-$1.tar.gz" \
+  && tar xf "$NAME-$1.tar.gz" \
+  && cd "$NAME-$1" \
+  && ./configure --prefix /usr \
+  && make -j4
+fi
+make install && ldconfig
