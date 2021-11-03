3ffec5362348c029b8839e9db204742b8f1a5004
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 17:12:44 +0200

    test

--- build_scripts/linux/libressl.sh
+++ build_scripts/linux/libressl.sh
@@ -4,10 +4,11 @@ if [[ -d "libressl-$1" ]]; then
   cd "libressl-$1" || exit 102
 else
   echo "No cache found for LibreSSL, build it..."
+  mkdir "libressl-$1"
   wget "https://github.com/libressl-portable/portable/archive/v$1.tar.gz" \
   && ls -la \
-  && tar xvf "v$1.tar.gz"  -C "libressl-$1" --strip-components 1 \
-  && ls -la \
+  && tar xvf "v$1.tar.gz" -C "libressl-$1" --strip-components 1 \
+  && ls -la "libressl-$1" \
   && cd "libressl-$1" \
   && ./autogen.sh \
   && ./configure \
