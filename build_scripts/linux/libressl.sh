20b2c72d2e6d5ae46b1a2c96d4b887889b7b8e20
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 13:47:32 +0200

    test

--- build_scripts/linux/libressl.sh
+++ build_scripts/linux/libressl.sh
@@ -5,7 +5,7 @@ if [[ -d "libressl-$1" ]]; then
 else
   echo "No cache found for LibreSSL, build it..."
   wget -O "libressl-$1.tar.gz" "https://github.com/libressl-portable/portable/archive/v$1.tar.gz" \
-  && tar xvf "libressl.tar.gz" --one-top-level \
+  && tar xvf "libressl-$1.tar.gz" --one-top-level \
   && echo $(ls -la)
 fi
 make install
