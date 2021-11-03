f125db9d174607860f43e3bc174844ebe7a28d0b
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 12:24:30 +0200

    test

--- build_scripts/linux/autoconf.sh
+++ build_scripts/linux/autoconf.sh
@@ -5,7 +5,7 @@ if [[ -d "autoconf-$1" ]]; then
 else
   echo "No cache found for autoconf, build it..."
   wget --no-check-certificate "https://ftp.gnu.org/gnu/autoconf/autoconf-$1.tar.gz" \
-  && tar "xvf autoconf-$1.tar.gz" \
+  && tar xvf "autoconf-$1.tar.gz" \
   && cd "autoconf-$1" \
   && ./configure \
   && make -j4
--- build_scripts/linux/automake.sh
+++ build_scripts/linux/automake.sh
@@ -1,15 +1,14 @@
 cd /host/build-tools || exit 2
-ls -la
-if [[ -d automake-"$1" ]]
-then
+if [[ -d "automake-$1" ]]; then
+  echo "Cache found for automake, install it..."
+  cd "automake-$1" || exit 102
+else
   echo "No cache found for automake, build it..."
-  wget --no-check-certificate https://ftp.gnu.org/gnu/autoconf/automake-"$1".tar.gz \
-  && tar xvf automake-"$1".tar.gz \
-  && cd automake-"$1" \
+  wget --no-check-certificate "https://ftp.gnu.org/gnu/autoconf/automake-$1.tar.gz" \
+  && tar xvf "automake-$1.tar.gz" \
+  && cd "automake-$1" \
   && ./configure \
   && make -j4
-else
-  echo "Cache found for automake, install it..."
 fi
 make install \
 && automake --version
