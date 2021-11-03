9b399661f9aae9af7e5cd3d504729b0c3cd7e698
Alexander Piskun <bigcat88@icloud.com>
2021-11-03 12:15:47 +0200

    test

--- build_scripts/linux/autoconf.sh
+++ build_scripts/linux/autoconf.sh
@@ -1,15 +1,14 @@
 cd /host/build-tools || exit 2
-ls -la
-if [[ -d autoconf-"$1" ]]
-then
+if [[ -d "autoconf-$1" ]]; then
+  echo "Cache found for autoconf, install it..."
+  cd "autoconf-$1" || exit 102
+else
   echo "No cache found for autoconf, build it..."
-  wget --no-check-certificate https://ftp.gnu.org/gnu/autoconf/autoconf-"$1".tar.gz \
-  && tar xvf autoconf-"$1".tar.gz \
-  && cd autoconf-"$1" \
+  wget --no-check-certificate "https://ftp.gnu.org/gnu/autoconf/autoconf-$1.tar.gz" \
+  && tar "xvf autoconf-$1.tar.gz" \
+  && cd "autoconf-$1" \
   && ./configure \
   && make -j4
-else
-  echo "Cache found for autoconf, install it..."
 fi
 make install \
 && autoconf --version
