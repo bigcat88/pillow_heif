613b44cd8814d3cd91753511ff7727fff737209a
Alexander Piskun <bigcat88@icloud.com>
2022-02-07 02:04:00 +0200

    dev

--- libheif/build.py
+++ libheif/build.py
@@ -1,4 +1,6 @@
-import os
+from os import getenv, path
+from sys import platform
+from subprocess import run, DEVNULL, PIPE
 from cffi import FFI
 
 ffibuilder = FFI()
@@ -10,10 +12,22 @@ with open("libheif/heif.h", "r", encoding="utf-8") as f:
 
 include_dirs = ["/usr/local/include", "/usr/include", "/opt/local/include"]
 library_dirs = ["/usr/local/lib", "/usr/lib", "/lib", "/opt/local/lib"]
-homebrew_prefix = os.getenv("HOMEBREW_PREFIX")
-if homebrew_prefix:
-    include_dirs.append(os.path.join(homebrew_prefix, "include"))
-    library_dirs.append(os.path.join(homebrew_prefix, "lib"))
+
+if platform.lower() == "darwin":
+    homebrew_prefix = getenv("HOMEBREW_PREFIX")
+    if not homebrew_prefix:
+        _result = run(['brew', '--prefix'], stderr=DEVNULL, stdout=PIPE, check=False)
+        if not _result.returncode and _result.stdout is not None:
+            homebrew_prefix = _result.stdout.decode('utf-8').rstrip('\n')
+    if homebrew_prefix:
+        homebrew_include = path.join(homebrew_prefix, "include")
+        if homebrew_include not in include_dirs:
+            include_dirs.append(homebrew_include)
+        homebrew_library = path.join(homebrew_prefix, "lib")
+        if homebrew_library not in library_dirs:
+            library_dirs.append(homebrew_library)
+    project_root = path.dirname(path.dirname(path.abspath(__file__)))
+    include_dirs.append(project_root)
 
 
 ffibuilder.set_source(
