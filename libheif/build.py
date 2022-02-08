955014a02869703ba949d3a39d0a78a8db29dae6
Alexander Piskun <bigcat88@icloud.com>
2022-02-08 16:14:53 +0200

    dev 0.1.6

--- libheif/build.py
+++ libheif/build.py
@@ -13,31 +13,22 @@ with open("libheif/heif.h", "r", encoding="utf-8") as f:
 include_dirs = ["/usr/local/include", "/usr/include", "/opt/local/include"]
 library_dirs = ["/usr/local/lib", "/usr/lib", "/lib", "/opt/local/lib"]
 
+include_path_prefix = ""
 if platform.lower() == "darwin":
-    homebrew_prefix = getenv("HOMEBREW_PREFIX")
-    if not homebrew_prefix:
+    include_path_prefix = getenv("HOMEBREW_PREFIX")
+    if not include_path_prefix:
         _result = run(["brew", "--prefix"], stderr=DEVNULL, stdout=PIPE, check=False)
         if not _result.returncode and _result.stdout is not None:
-            homebrew_prefix = _result.stdout.decode("utf-8").rstrip("\n")
-    if homebrew_prefix:
-        homebrew_include = path.join(homebrew_prefix, "include")
-        if homebrew_include not in include_dirs:
-            include_dirs.append(homebrew_include)
-        homebrew_library = path.join(homebrew_prefix, "lib")
-        if homebrew_library not in library_dirs:
-            library_dirs.append(homebrew_library)
-lib_path = ""
-if platform.lower() == "win32":
-    libheif_path = getenv("PH_LIB_PATH")
-    if libheif_path:
-        a = path.join(libheif_path, "include")
-        if a not in include_dirs:
-            print(f"Adding {a}")
-            include_dirs.append(a)
-        lib_path = path.join(libheif_path, "lib")
-        if lib_path not in library_dirs:
-            print(f"Adding {lib_path}")
-            library_dirs.append(lib_path)
+            include_path_prefix = _result.stdout.decode("utf-8").rstrip("\n")
+elif platform.lower() == "win32":
+    include_path_prefix = getenv("VCPKG_PREFIX")
+if include_path_prefix:
+    include_path_prefix_include = path.join(include_path_prefix, "include")
+    if include_path_prefix_include not in include_dirs:
+        include_dirs.append(include_path_prefix_include)
+    include_path_prefix_lib = path.join(include_path_prefix, "lib")
+    if include_path_prefix_lib not in library_dirs:
+        library_dirs.append(include_path_prefix_lib)
 if platform.lower() in ("darwin", "win32"):
     project_root = path.dirname(path.dirname(path.abspath(__file__)))
     include_dirs.append(project_root)
@@ -52,7 +43,6 @@ ffibuilder.set_source(
     library_dirs=library_dirs,
     libraries=["heif"],
     extra_compile_args=["/d2FH4-"] if platform.lower() == "win32" else [],
-    extra_link_args=["/VERBOSE:LIB"],
 )
 
 if __name__ == "__main__":
