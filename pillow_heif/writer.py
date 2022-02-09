52cf7f64ba73fa9c3ae08499f7cf40d28e36a365
Alexander Piskun <bigcat88@icloud.com>
2022-02-10 00:26:28 +0200

    0.1.6

--- pillow_heif/writer.py
+++ pillow_heif/writer.py
@@ -4,4 +4,4 @@ Write exif tags, change rotation and other stuff. Currently not available.
 
 
 def write_heif():
-    raise Exception("not implemented")
+    raise NotImplementedError("not done yet")
