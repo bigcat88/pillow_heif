58a9b3a79946c87551bd45d14c9063260272252c
Alexander Piskun <bigcat88@icloud.com>
2022-02-10 02:12:52 +0200

    0.1.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,31 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.1.6 - 2022-02-10]
+
+### Added
+
+- Windows binary wheels.
+- More tests to `as_opener` module.
+- Code coverage.
+- Linux binary wheels for Python 3.6(this is last release for Python 3.6).
+
+### Changed
+
+- Using code formatting: black.
+- Started changing build algorithms to support PEP 517.
+- Speed optimizations and adjustments to `as_opener` module.
+- Added `info[icc_profile]` to `HeifImageFile` when use it as `as_opener`.
+- Adjustments to `reader` module:
+- `pillow_heif.open` is in process of deprecation, still available, but you should use `open_heif` instead.
+- `pillow_heif.check` is in process of deprecation, still available, but you should use `check_heif` instead.
+- `pillow_heif.read` is in process of deprecation, still available, but you should use `read_heif` instead.
+
+### Fixed
+
+- Apple M1 binary wheels now build on Monterey 12.01 instead of 12.2.
+- HeifError exception class now calls `super` to init all values.
+
 ## [0.1.5 - 2022-02-01]
 
 ### Added
@@ -23,7 +48,7 @@ All notable changes to this project will be documented in this file.
 
 - GitHub build Actions now use cache.
 - Added `libaom` library to linux build.
-- More tests. 
+- More tests.
 
 ### Changed
 
@@ -31,7 +56,7 @@ All notable changes to this project will be documented in this file.
 
 ### Fixed
 
-- Bug with header check when used as plugin for Pillow with `as_opener` function. Thanks for this to @DimonLavron 
+- Bug with header check when used as plugin for Pillow with `as_opener` function. Thanks for this to @DimonLavron
 
 ## [0.1.3 - 2021-10-25]
 
--- libheif/build.py
+++ libheif/build.py
@@ -3,42 +3,47 @@ from sys import platform
 from subprocess import run, DEVNULL, PIPE
 from cffi import FFI
 
-ffibuilder = FFI()
+ffi = FFI()
 
 
 with open("libheif/heif.h", "r", encoding="utf-8") as f:
-    ffibuilder.cdef(f.read())
+    ffi.cdef(f.read())
 
 
 include_dirs = ["/usr/local/include", "/usr/include", "/opt/local/include"]
 library_dirs = ["/usr/local/lib", "/usr/lib", "/lib", "/opt/local/lib"]
 
+if platform.lower() in ("darwin", "win32"):
+    include_dirs.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+include_path_prefix = ""
 if platform.lower() == "darwin":
-    homebrew_prefix = getenv("HOMEBREW_PREFIX")
-    if not homebrew_prefix:
-        _result = run(['brew', '--prefix'], stderr=DEVNULL, stdout=PIPE, check=False)
+    include_path_prefix = getenv("HOMEBREW_PREFIX")
+    if not include_path_prefix:
+        _result = run(["brew", "--prefix"], stderr=DEVNULL, stdout=PIPE, check=False)
         if not _result.returncode and _result.stdout is not None:
-            homebrew_prefix = _result.stdout.decode('utf-8').rstrip('\n')
-    if homebrew_prefix:
-        homebrew_include = path.join(homebrew_prefix, "include")
-        if homebrew_include not in include_dirs:
-            include_dirs.append(homebrew_include)
-        homebrew_library = path.join(homebrew_prefix, "lib")
-        if homebrew_library not in library_dirs:
-            library_dirs.append(homebrew_library)
-    project_root = path.dirname(path.dirname(path.abspath(__file__)))
-    include_dirs.append(project_root)
+            include_path_prefix = _result.stdout.decode("utf-8").rstrip("\n")
+elif platform.lower() == "win32":
+    include_path_prefix = getenv("VCPKG_PREFIX")
+if include_path_prefix:
+    include_path_prefix_include = path.join(include_path_prefix, "include")
+    if include_path_prefix_include not in include_dirs:
+        include_dirs.append(include_path_prefix_include)
+    include_path_prefix_lib = path.join(include_path_prefix, "lib")
+    if include_path_prefix_lib not in library_dirs:
+        library_dirs.append(include_path_prefix_lib)
 
 
-ffibuilder.set_source(
-    "pillow_heif._libheif",
+ffi.set_source(
+    "pillow_heif.libheif",
     """
      #include "libheif/heif.h"
     """,
     include_dirs=include_dirs,
     library_dirs=library_dirs,
     libraries=["heif"],
+    extra_compile_args=["/d2FH4-"] if platform.lower() == "win32" else [],
 )
 
 if __name__ == "__main__":
-    ffibuilder.compile(verbose=True)
+    ffi.compile(verbose=True)
