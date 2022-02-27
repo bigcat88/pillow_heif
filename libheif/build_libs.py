a47f8f4b6514783ff165158597f301a3be3bc5ac
Alexander Piskun <bigcat88@icloud.com>
2022-02-27 17:32:05 +0200

    0.1.7

--- libheif/build_libs.py
+++ libheif/build_libs.py
@@ -135,10 +135,10 @@ def build_lib_linux(url: str, name: str, musl: bool = False):
         if name == "libde265":
             run(["./autogen.sh"], check=True)
         if name == "aom":
-            cmake_args = "-DENABLE_TESTS=0 -DENABLE_TOOLS=0 -DENABLE_EXAMPLES=0 -DENABLE_DOCS=0".split()
-            cmake_args += "-DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=1".split()
-            cmake_args += f"-DCMAKE_INSTALL_PREFIX={INSTALL_DIR_LIBS} ../aom".split()
-            run(["cmake"] + cmake_args, check=True)
+            cmake_args = "-DENABLE_TESTS=0 -DENABLE_TOOLS=0 -DENABLE_EXAMPLES=0 -DENABLE_DOCS=0"
+            cmake_args += "-DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=1"
+            cmake_args += f"-DCMAKE_INSTALL_PREFIX={INSTALL_DIR_LIBS} ../aom"
+            run(f"cmake {cmake_args}", shell=True, check=True)
         else:
             configure_args = f"--prefix {INSTALL_DIR_LIBS}".split()
             if name == "libde265":
