3c870bd59b1b49ec1c2ff94efe8be3098064f12c
Alexander Piskun <bigcat88@icloud.com>
2022-03-14 01:30:10 +0200

    dev

--- libheif/build_libs.py
+++ libheif/build_libs.py
@@ -115,7 +115,7 @@ def build_tools_linux(musl: bool = False):
     )
     build_tool_linux("https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.gz", "autoconf", "2.71")
     build_tool_linux("https://ftp.gnu.org/gnu/automake/automake-1.16.5.tar.gz", "automake", "1.16.5")
-    build_tool_linux("https://github.com/Kitware/CMake/archive/refs/tags/v3.22.1.tar.gz", "cmake", "3.16.1")
+    build_tool_linux("https://github.com/Kitware/CMake/archive/refs/tags/v3.22.3.tar.gz", "cmake", "3.16.1")
     build_tool_linux(
         "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.gz", "nasm", "2.15.05", chmod="774"
     )
