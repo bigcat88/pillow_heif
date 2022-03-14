7cff87988a737f28ccb0c2c34cb2c14e9e15d8d4
Alexander Piskun <bigcat88@icloud.com>
2022-03-14 17:07:23 +0200

    dev

--- libheif/build_libs.py
+++ libheif/build_libs.py
@@ -152,10 +152,10 @@ def build_lib_linux(url: str, name: str, musl: bool = False):
             cmake_args = "-DENABLE_TESTS=0 -DENABLE_TOOLS=0 -DENABLE_EXAMPLES=0 -DENABLE_DOCS=0".split()
             cmake_args += "-DENABLE_TESTDATA=0 -DCONFIG_AV1_ENCODER=0".split()
             cmake_args += "-DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=1".split()
-            cmake_args += f"-DCMAKE_INSTALL_PREFIX={INSTALL_DIR_LIBS} ../aom".split()
+            cmake_args += f"/W1 -DCMAKE_INSTALL_PREFIX={INSTALL_DIR_LIBS} ../aom".split()
             run(["cmake"] + cmake_args, check=True)
         elif name == "x265":
-            cmake_args = "-DCMAKE_INSTALL_PREFIX={INSTALL_DIR_LIBS} ./source".split()
+            cmake_args = f"/W1 -DCMAKE_INSTALL_PREFIX={INSTALL_DIR_LIBS} ./source".split()
             cmake_args += ["-G", "Unix Makefiles"]
             run(["cmake"] + cmake_args, check=True)
         else:
@@ -207,3 +207,8 @@ def build_libs_linux():
     finally:
         chdir(_original_dir)
     return INSTALL_DIR_LIBS
+
+
+if __name__ == "__main__":
+    build_libs_linux()
+    exit(0)
