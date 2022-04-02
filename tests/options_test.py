f335e5010153a1c48169d98cee364d81472c8517
Alexander Piskun <bigcat88@icloud.com>
2022-04-02 21:54:49 +0300

    dev 0.2.0 [ci skip]

--- tests/options_test.py
+++ tests/options_test.py
@@ -1,8 +1,6 @@
-import builtins
 import os
-from json import load
+from io import SEEK_END, BytesIO
 from pathlib import Path
-from warnings import warn
 
 import pytest
 from PIL import Image, UnidentifiedImageError
@@ -12,64 +10,56 @@ from pillow_heif import open_heif, options, register_heif_opener
 register_heif_opener()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-with builtins.open("images_info.json", "rb") as _:
-    all_images = load(_)
 
-if not options().avif:
-    warn("Skipping tests for `AV1` format due to lack of codecs.")
-    all_images = [e for e in all_images if not e["name"].endswith(".avif")]
-
-invalid_images = [e for e in all_images if not e["valid"]]
-heif_images = [e for e in all_images if e["valid"]]
-
-heic_images = [e for e in heif_images if e["name"].endswith(".heic")]
-hif_images = [e for e in heif_images if e["name"].endswith(".hif")]
-avif_images = [e for e in heif_images if e["name"].endswith(".avif")]
-
-
-@pytest.mark.parametrize("img_info_list", [heic_images[:2] + hif_images[:2] + avif_images[:2]])
-def test_avif_cfg_option(img_info_list):
+@pytest.mark.skipif(not options().avif, reason="missing AV1 codecs.")
+def test_avif_cfg_option():
     try:
         options().avif = False
-        avif_files = skipped_files = 0
-        for img_info in img_info_list:
-            try:
-                if img_info["name"].endswith(".avif"):
-                    avif_files += 1
-                Image.open(Path(img_info["file"]))
-            except UnidentifiedImageError:
-                skipped_files += 1
-        assert skipped_files == avif_files
+        for file in list(Path().glob("images/avif/*.avif")):
+            with pytest.raises(UnidentifiedImageError):
+                Image.open(file)
     finally:
         options().reset()
 
 
-@pytest.mark.parametrize("img_info", [e for e in heif_images if not e["strict"]])
-def test_strict_cfg_option(img_info):
+def test_strict_cfg_option():
+    image_path = Path("images/nokia/bird_burst.heic")
     try:
         options().update(strict=True)
         with pytest.raises(UnidentifiedImageError):
-            Image.open(Path(img_info["file"]))
+            Image.open(image_path)
         options().update(strict=False)
-        Image.open(Path(img_info["file"]))
+        Image.open(image_path)
     finally:
         options().reset()
 
 
-@pytest.mark.parametrize("img_info", hif_images[:1])
-def test_thumbnails_option(img_info):
+def test_thumbnails_option():
+    image_path = Path("images/pug_1_1.heic")
     try:
-        heif_file = open_heif(Path(img_info["file"]))
+        heif_file = open_heif(image_path)
+        assert heif_file.thumbnails
+        assert str(heif_file.thumbnails[0]).find("bytes thumbnail image data") == -1
+        assert heif_file.thumbnails[0].data
+        assert str(heif_file.thumbnails[0]).find("bytes thumbnail image data") != -1
+        heif_file.close()
+        options().thumbnails = False
+        heif_file = open_heif(image_path)
         assert not heif_file.thumbnails
         heif_file.close()
-        options().thumbnails = True
-        heif_file = open_heif(Path(img_info["file"]))
-        assert heif_file.thumbnails
-        assert not heif_file.thumbnails[0].data
-        heif_file.close()
-        options().thumbnails_autoload = True
-        heif_file = open_heif(Path(img_info["file"]))
-        assert heif_file.thumbnails[0].data
-        assert str(heif_file.thumbnails[0]).find("HeifThumbnail") != -1
+    finally:
+        options().reset()
+
+
+def test_quality_option():
+    try:
+        image = open_heif(Path("images/pug_1_1.heic"))
+        options().quality = 10
+        out_buf1 = BytesIO()
+        image.save(out_buf1)
+        options().quality = 30
+        out_buf2 = BytesIO()
+        image.save(out_buf2)
+        assert out_buf1.seek(0, SEEK_END) < out_buf2.seek(0, SEEK_END)
     finally:
         options().reset()
