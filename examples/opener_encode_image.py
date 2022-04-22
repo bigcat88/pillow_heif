1969c028048f0297f0eedd9c6049c466e4317f06
Alexander Piskun <bigcat88@icloud.com>
2022-04-22 22:39:21 +0300

    dev 0.2.2

--- examples/opener_encode_image.py
+++ examples/opener_encode_image.py
@@ -1,25 +1,18 @@
 import os
-import sys
-import traceback
 from pathlib import Path
 
 from PIL import Image
 
 import pillow_heif
 
+os.chdir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tests"))
+TARGET_FOLDER = "../converted"
+
 # This demo converts jpeg to heic with low quality.
 if __name__ == "__main__":
-    # Change directory to project root.
-    os.chdir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tests"))
     pillow_heif.register_heif_opener()
-    target_folder = "../converted"
-    os.makedirs(target_folder, exist_ok=True)
+    os.makedirs(TARGET_FOLDER, exist_ok=True)
     image_path = Path("images/jpeg_gif_png/pug_90_flipped.jpeg")
-    try:
-        img = Image.open(image_path)
-        img.save(os.path.join(target_folder, f"{image_path.stem}.heic"), quality=50)
-        img.close()
-    except Exception as e:
-        print(f"{repr(e)} during processing {image_path.as_posix()}", file=sys.stderr)
-        print(traceback.format_exc())
-    exit(0)
+    img = Image.open(image_path)
+    img.save(os.path.join(TARGET_FOLDER, f"{image_path.stem}.heic"), quality=50)
+    img.close()
--- examples/reader_create_image.py
+++ examples/reader_create_image.py
@@ -5,15 +5,14 @@ from PIL import Image
 
 import pillow_heif
 
+os.chdir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tests"))
+TARGET_FOLDER = "../converted"
+
 # This demo creates `three_images.heic` from `rgba10bit.png`, `rgb8_512_512_1_0.heic` and `rgb8_512_512_1_2.heic`.
 if __name__ == "__main__":
-    # Change directory to project root.
-    os.chdir(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tests"))
-    target_folder = "../converted"
-    os.makedirs(target_folder, exist_ok=True)
+    os.makedirs(TARGET_FOLDER, exist_ok=True)
     pillow_pug_jpeg = Image.open(Path("images/jpeg_gif_png/rgba10bit.png"))
     out_heif_container = pillow_heif.from_pillow(pillow_pug_jpeg)
     out_heif_container.add_from_heif(pillow_heif.open_heif(Path("images/rgb8_512_512_1_0.heic")))
     out_heif_container.add_from_heif(pillow_heif.open_heif(Path("images/rgb8_512_512_1_2.heic")))
     out_heif_container.save(Path("../converted/three_images.heic"), quality=90)
-    exit(0)
