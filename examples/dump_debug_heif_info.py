8a76c90b1a26b74251790cfbde936a43bd5ecb33
Alexander Piskun <bigcat88@icloud.com>
2022-04-22 18:48:00 +0300

    added dump of Exif, XMP, ICC

--- examples/dump_debug_heif_info.py
+++ examples/dump_debug_heif_info.py
@@ -1,9 +1,14 @@
 import sys
+from io import BytesIO
+
+import piexif
+from PIL import ImageCms
 
 import pillow_heif
 
 if __name__ == "__main__":
     file = sys.argv[1]
+    # file = "../tests/images/rgb8_128_128_2_1.heic"
     print("Dumping info for file:", file)
     print("Check result:", pillow_heif.HeifFiletype(pillow_heif.check_heif(file)))
     print("Supported:", pillow_heif.is_supported(file))
@@ -15,7 +20,7 @@ if __name__ == "__main__":
     # print(f"Saving debug boxes to {boxes_name}")
     # heif_file._debug_dump(boxes_name)  # noqa
     print("Number of images:", len(heif_file))
-    print("Number of thumbnails:", len([i for i in heif_file.thumbnails_all()]))
+    print("Number of thumbnails:", len(list(heif_file.thumbnails_all())))
     print("Information about each image:")
     for image in heif_file:
         print("\tID:", image.info["img_id"])
@@ -31,13 +36,33 @@ if __name__ == "__main__":
         print("\tChroma:", image.chroma)
         print("\tColor:", image.color)
         if image.info.get("icc_profile", None) is not None:
-            print("\tICC:", "TODO")
-        elif image.info.get("nclx_profile", None):
+            if len(image.info["icc_profile"]):
+                icc_profile = BytesIO(image.info["icc_profile"])
+                print("\tICC:", str(ImageCms.getProfileDescription(icc_profile)).strip())
+            else:
+                print("\tICC: Empty")
+        if image.info.get("nclx_profile", None):
             print("\tNCLX:", "TODO")
         if image.info.get("exif", None):
-            print("\tExif:", "TODO")
+            print("\tExif:")
+            exif_dict = piexif.load(image.info["exif"], key_is_name=True)
+            for key, value in exif_dict.items():
+                print(f"\t\t{key}:")
+                if value is not None:
+                    for sub_key, sub_value in value.items():
+                        if isinstance(sub_value, bytes) and len(sub_value) > 20:
+                            print(f"\t\t\t{sub_key}: {len(sub_value)} bytes.")
+                        else:
+                            print(f"\t\t\t{sub_key}: {sub_value}")
+        if image.info.get("xmp", None):
+            print("\tXmp:")
+            print("\t\t", pillow_heif.getxmp(image.info["xmp"]))
         if image.info.get("metadata", None):
-            print("\tMetadata:", "TODO")
+            print("\tMetadata:")
+            for block in image.info["metadata"]:
+                print("\t\tType:", block["type"])
+                print("\t\tcontent_type:", block["content_type"])
+                print("\t\tData length:", len(block["data"]))
         print("\tThumbnails:")
         for thumbnail in image.thumbnails:
             print("\t\tID:", thumbnail.info["thumb_id"])
