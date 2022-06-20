db311cd1e92ea76738edf4009c076e0eedc8ea90
Alexander Piskun <bigcat88@icloud.com>
2022-06-21 01:53:06 +0300

    dev[ci ckip]

--- pillow_heif/private.py
+++ pillow_heif/private.py
@@ -59,7 +59,7 @@ def copy_image_data(dest_data, src_data, dest_stride: int, source_stride: int, h
     else:
         p_source = ffi.from_buffer("uint8_t*", src_data)
         for i in range(height):
-            ffi.memmove(dest_data + dest_stride * i, p_source + source_stride * i, dest_stride)
+            ffi.memmove(dest_data + dest_stride * i, p_source + source_stride * i, source_stride)
 
 
 def read_color_profile(handle) -> dict:
