9ab484ef6e39e4f2b5074df7ff9d75564df53fad
Alexander Piskun <bigcat88@icloud.com>
2022-07-04 17:00:57 +0300

    PEP 425

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel
+
+
+class Abi3Wheels(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+        if python.startswith("cp"):
+            return "cp36", "abi3", plat
+        return python, abi, plat
 
 
 def get_version():
@@ -12,4 +21,5 @@ def get_version():
 setup(
     version=get_version(),
     cffi_modules=["libheif/build.py:ffi"],
+    cmdclass={"bdist_wheel": Abi3Wheels},
 )
