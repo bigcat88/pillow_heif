45612904738f248dab54f0be08b047c5360d7664
Alexander Piskun <bigcat88@icloud.com>
2022-07-07 09:19:15 +0300

    CPython `abi3` wheels.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel
+
+
+class WheelsABI3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+        if python.startswith("cp"):
+            python = "cp36"
+            abi = "abi3"
+            if plat.startswith("macosx"):
+                python = "cp37" if plat.find("x86_64") != -1 else "cp39"
+            elif plat.startswith("win"):
+                python = "cp37"
+            elif plat == "linux_armv7l":
+                python = "cp38"
+        return python, abi, plat
 
 
 def get_version():
@@ -12,4 +28,5 @@ def get_version():
 setup(
     version=get_version(),
     cffi_modules=["libheif/build.py:ffi"],
+    cmdclass={"bdist_wheel": WheelsABI3},
 )
