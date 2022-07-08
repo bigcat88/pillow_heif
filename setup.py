ee6eddbb68e91b740856ef6c5ed075b94942a2ae
Alexander Piskun <bigcat88@icloud.com>
2022-07-08 14:55:02 +0300

    dev(armv7l  wheels) [ci skip]

--- setup.py
+++ setup.py
@@ -2,20 +2,29 @@
 from setuptools import setup
 from wheel.bdist_wheel import bdist_wheel
 
+# class WheelsABI3(bdist_wheel):
+#     def get_tag(self):
+#         python, abi, plat = super().get_tag()
+#         if python.startswith("cp"):
+#             python = "cp36"
+#             abi = "abi3"
+#             if plat.startswith("macosx"):
+#                 python = "cp37" if plat.find("x86_64") != -1 else "cp39"
+#             elif plat.startswith("win"):
+#                 python = "cp37"
+#             elif plat == "linux_armv7l":
+#                 python = "cp38"
+#         return python, abi, plat
 
-class WheelsABI3(bdist_wheel):
+
+class WheelNoneABI(bdist_wheel):
+    def finalize_options(self):
+        bdist_wheel.finalize_options(self)
+        self.root_is_pure = False  # noqa
+
     def get_tag(self):
-        python, abi, plat = super().get_tag()
-        if python.startswith("cp"):
-            python = "cp36"
-            abi = "abi3"
-            if plat.startswith("macosx"):
-                python = "cp37" if plat.find("x86_64") != -1 else "cp39"
-            elif plat.startswith("win"):
-                python = "cp37"
-            elif plat == "linux_armv7l":
-                python = "cp38"
-        return python, abi, plat
+        python, abi, plat = bdist_wheel.get_tag(self)
+        return "py3", "none", plat
 
 
 def get_version():
@@ -28,5 +37,6 @@ def get_version():
 setup(
     version=get_version(),
     cffi_modules=["libheif/build.py:ffi"],
-    cmdclass={"bdist_wheel": WheelsABI3},
+    cmdclass={"bdist_wheel": WheelNoneABI},
+    # cmdclass={"bdist_wheel": WheelsABI3},
 )
