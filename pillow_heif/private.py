a20c6114f44ba69da1ee2c934c31911a2a8745b0
Alexander Piskun <bigcat88@icloud.com>
2022-07-16 14:24:26 +0300

    fix invalid pillow version comparison

--- pillow_heif/private.py
+++ pillow_heif/private.py
@@ -206,7 +206,7 @@ def set_metadata(ctx: LibHeifCtxWrite, heif_img_handle, info: dict) -> None:
 def exif_from_pillow(additional_info: dict, frame) -> None:
     if "exif" not in additional_info:
         if hasattr(frame, "getexif"):
-            if pil_version[:2] not in ("6.", "7.", "8.") or pil_version[:4] not in ("9.0.", "9.1."):
+            if pil_version[:2] not in ("6.", "7.", "8.") and pil_version[:4] not in ("9.0.", "9.1."):
                 exif = frame.getexif()
                 if exif:
                     additional_info["exif"] = exif.tobytes()
