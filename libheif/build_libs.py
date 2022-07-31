31acc7dba2f41dc1666b86d6cc28e9f552b6ecc0
Alexander Piskun <bigcat88@icloud.com>
2022-07-31 20:37:18 +0300

    dev 0.6.0 [ci skip]

--- libheif/build_libs.py
+++ libheif/build_libs.py
@@ -187,6 +187,7 @@ def build_lib_linux(url: str, name: str, musl: bool = False):
             configure_args = f"--prefix {INSTALL_DIR_LIBS}".split()
             if name == "libde265":
                 configure_args += "--disable-sherlock265 --disable-dec265 --disable-dependency-tracking".split()
+                configure_args += 'LDFLAGS="-lstdc++ -lm -lc -lgcc_s"'.split()
             elif name == "libheif":
                 configure_args += "--disable-examples --disable-go --disable-gdk-pixbuf".split()
             run(["./configure"] + configure_args, check=True)
