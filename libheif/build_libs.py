1d215aa9bf8120f8c71d7f99e15e2ace3e97d752
Alexander Piskun <bigcat88@icloud.com>
2022-08-07 19:57:48 +0300

    dev 0.6.1 [ci skip]

--- libheif/build_libs.py
+++ libheif/build_libs.py
@@ -165,6 +165,8 @@ def build_lib_linux(url: str, name: str, musl: bool = False):
             else:
                 download_extract_to(url, _lib_path)
                 if name == "libde265":
+                    print("DEBUG1:", path.join(_lib_path, "libde265"))
+                    run(f"ls -la {path.join(_lib_path, 'libde265')}".split(), check=True)
                     chdir(path.join(_lib_path, "libde265"))
                     for patch in (
                         "libde265/CVE-2022-1253.patch",
