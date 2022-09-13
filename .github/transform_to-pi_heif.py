db8736a4fbfc78a9d9dddb6eade947795da57adc
Alexander Piskun <bigcat88@icloud.com>
2022-09-13 09:26:25 +0300

    dev 0.7.0

--- .github/transform_to-pi_heif.py
+++ .github/transform_to-pi_heif.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
         "libheif/build.py",
         "setup.py",
         "docker/armv7l_wheel.Dockerfile",
+        "docker/test_wheels.Dockerfile",
         "MANIFEST.in",
     ]
     for dir_name in ("pillow_heif", "tests"):
