f10db37c8e3f6d85221f4de248e0051159633424
Alexander Piskun <bigcat88@icloud.com>
2022-11-20 02:22:46 +0300

    dev: libheif 1.14.0(0) - macos

--- pi-heif/libheif/macos/libheif.rb
+++ pi-heif/libheif/macos/libheif.rb
@@ -3,8 +3,8 @@
 class Libheif < Formula
   desc "ISO/IEC 23008-12:2017 HEIF file format decoder and encoder"
   homepage "https://www.libde265.org/"
-  url "https://github.com/strukturag/libheif/releases/download/v1.13.0/libheif-1.13.0.tar.gz"
-  sha256 "c20ae01bace39e89298f6352f1ff4a54b415b33b9743902da798e8a1e51d7ca1"
+  url "https://github.com/strukturag/libheif/releases/download/v1.14.0/libheif-1.14.0.tar.gz"
+  sha256 "9a2b969d827e162fa9eba582ebd0c9f6891f16e426ef608d089b1f24962295b5"
   license "LGPL-3.0-only"
   # Set current revision from what it was taken plus 10
   revision 10
