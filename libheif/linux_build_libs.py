da2e2cb8985330b7bd456f4c5ac7e1ba3e3a173a
bigcat88 <bigcat88@icloud.com>
2025-05-24 09:09:23 +0300

    chore(libheif,libde265): updated versions
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- libheif/linux_build_libs.py
+++ libheif/linux_build_libs.py
@@ -12,8 +12,8 @@ INSTALL_DIR_LIBS = environ.get("INSTALL_DIR_LIBS", "/usr")
 PH_LIGHT_VERSION = sys.maxsize <= 2**32 or getenv("PH_LIGHT_ACTION", "0") != "0"
 
 LIBX265_URL = "https://bitbucket.org/multicoreware/x265_git/get/0b75c44c10e605fe9e9ebed58f04a46271131827.tar.gz"
-LIBDE265_URL = "https://github.com/strukturag/libde265/releases/download/v1.0.15/libde265-1.0.15.tar.gz"
-LIBHEIF_URL = "https://github.com/strukturag/libheif/releases/download/v1.19.7/libheif-1.19.7.tar.gz"
+LIBDE265_URL = "https://github.com/strukturag/libde265/releases/download/v1.0.16/libde265-1.0.16.tar.gz"
+LIBHEIF_URL = "https://github.com/strukturag/libheif/releases/download/v1.19.8/libheif-1.19.8.tar.gz"
 
 
 def download_file(url: str, out_path: str) -> bool:
