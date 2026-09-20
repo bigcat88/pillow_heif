4ed1988a8f87b7a9b8543fa567ed2e0afbe37872
BetoFernandez123 <127429452+BetoFernandez123@users.noreply.github.com>
2026-09-20 09:07:59 +0100

    Fix import statement for register_heif_opener
    Pylance in VS Code flag register_heif_opener as an internal function: "register_heif_opener" is not exported from module "pillow_heif"
      Import from "pillow_heif.as_plugin" instead
    
    Signed-off-by: BetoFernandez123 <127429452+BetoFernandez123@users.noreply.github.com>

--- pillow_heif/__init__.py
+++ pillow_heif/__init__.py
@@ -5,7 +5,7 @@ from ._lib_info import libheif_info, libheif_version
 from ._version import __version__
 from .as_plugin import (
     HeifImageFile,
-    register_heif_opener,
+    register_heif_opener as register_heif_opener,
 )
 from .constants import (
     HeifColorPrimaries,
